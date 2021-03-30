1fba3be2c7cd1ca2455a13c6eebda8b96df37905
Exists <55012346+userterminal@users.noreply.github.com>
2021-03-30 16:32:12 -0500

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 import { serve } from "https://deno.land/x/sift/mod.ts";
 
 serve({
-  "/":() => fetch(new URL("./static.json", import.meta.url))
+  "/":() => fetch(new URL("./static.json", import.meta.url)),
+  "/png":() => fetch(new URL("./hello.png", import.meta.url)),
 });
