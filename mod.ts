8c8b6afc7ce1bdb9a82e8b5d7861bcd2a51c3098
Exists <55012346+userterminal@users.noreply.github.com>
2021-03-30 16:32:58 -0500

    fix?

--- mod.ts
+++ mod.ts
@@ -2,5 +2,5 @@ import { serve } from "https://deno.land/x/sift/mod.ts";
 
 serve({
   "/":() => fetch(new URL("./static.json", import.meta.url)),
-  "/png":() => fetch(new URL("./hello.png", import.meta.url)),
+  "/png":() => fetch(new URL("./hello.PNG", import.meta.url)),
 });
